bad8d0b3811837179dbd779d6ab2f5f69e901a0d
Vortetty <33466216+Vortetty@users.noreply.github.com>
2020-09-18 12:51:19 -0700

    Update compiler.py

--- c2logic/compiler.py
+++ c2logic/compiler.py
@@ -536,11 +536,7 @@ def get_include_path():
 		raise ValueError(f"Unknown os {os.name}")
 
 def main(file, optimization = 0):
-	args = {
-		"file": file,
-		"optimization_level": optimization
-	}
-	print(Compiler(args.optimization_level).compile(args.file), file=args.file+"masm")
+	print(Compiler(optimization).compile(file), file=file+"masm")
 
 if __name__ == "__main__":
 	main()
